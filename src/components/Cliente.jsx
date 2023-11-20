d1985d9fa6204e5de43422ac9071ac3014795d6d
Richardod01 <julio202011329@ixtapaluca.tecnm.mx>
2023-11-20 16:45:35 -0600

    Agregamos un nuevo componente

---
+++ src/components/Cliente.jsx
@@ -0,0 +1,16 @@
+function Cliente({ cliente }) {
+
+    const { nombre, empresa, email, telefono, id } = cliente
+
+    return (
+        <>
+            <tr>
+                <td className="p-6">
+                    {nombre}
+                </td>
+            </tr>
+        </>
+    )
+}
+
+export default Cliente
