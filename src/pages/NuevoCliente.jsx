22482a59942fd34be95c35f92c52bd1e41a713db
Richardod01 <julio202011329@ixtapaluca.tecnm.mx>
2023-11-22 10:18:09 -0600

    Agregamos un nuevo componentes y validaiones de formularios con useActionData

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,14 +1,37 @@
-import { useNavigate, Form } from "react-router-dom"
+import { useNavigate, Form, useActionData } from "react-router-dom"
 import Formulario from "../components/Formulario";
+import Error from "../components/Error";
 
-export function action() {
-  console.log('Submit al formulario...')
-  return null;
+export async function action({ request }) {
+  const formData = await request.formData();
+  const datos = Object.fromEntries(formData);
+  const email = formData.get('email')
+  //Validacion de los datos
+  
+  const errores = []
+  if (Object.values(datos).includes('')) {
+    errores.push('Todos los campos son obligatorios');
+  }
+  
+  let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+  if(!regex.test(email)){
+    errores.push('Correo invalido')
+  }
+  
+  //Retornar datos si hay errores
+  
+  if (Object.keys(errores).length) {
+    return errores;
+  }
+  return 0
 }
 
 function NuevoCliente() {
 
+  const errores = useActionData();
   const navigate = useNavigate();
+
+  console.log(errores)
   return (
     <>
       <h1 className='font-black text-4xl text-blue-900'>Nuevo Cliente</h1>
@@ -23,14 +46,16 @@ function NuevoCliente() {
       </div>
 
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+        {errores?.length && errores.map((error, i) => <Error key={i}>{error}</Error>)}
         <Form
-          method = 'post'
+          method='post'
+          noValidate
         >
           <Formulario />
           <input
             type="submit"
             className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
-            value= "Registrar Cliente"
+            value="Registrar Cliente"
           />
         </Form>
       </div>
