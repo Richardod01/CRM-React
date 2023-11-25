b7a236107b75a2c81ab6046adb35ab5eef48b486
Richardod01 <julio202011329@ixtapaluca.tecnm.mx>
2023-11-25 09:50:55 -0600

    Agregamoe el componente de errores hacemos fecth y post al server json y algunas otras modificaciones que aun no recuerdo

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout'
 import NuevoCliente, { action as nuevoClienteAction } from './pages/NuevoCliente'
 import Index, { loader as clientesLoader} from './pages/Index'
+import ErrorPage from './components/ErrorPage'
 
 
 const router = createBrowserRouter([
@@ -15,7 +16,8 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Index />,
-        loader: clientesLoader
+        loader: clientesLoader,
+        errorElement: <ErrorPage/>
       },
       {
         path: '/clientes/nuevo',
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,6 +1,7 @@
 import { useNavigate, Form, useActionData } from "react-router-dom"
 import Formulario from "../components/Formulario";
 import Error from "../components/Error";
+import { agregarCliente } from "../data/Clientes";
 
 export async function action({ request }) {
   const formData = await request.formData();
@@ -23,6 +24,9 @@ export async function action({ request }) {
   if (Object.keys(errores).length) {
     return errores;
   }
+
+  agregarCliente(datos)
+
   return 0
 }
 
